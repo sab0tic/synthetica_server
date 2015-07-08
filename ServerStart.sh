cbfc35b25ac8122ef7e641614d9f1564c2d41b7c
sab0tic <cf.15094@gmail.com>
2015-07-07 19:27:07 -0600

    updated config tweaks, recipe script, start script

--- ServerStart.sh
+++ ServerStart.sh
@@ -2,12 +2,16 @@
 
 cd "$(dirname "$0")"
 
-
 # makes things easier if script needs debugging
 if [ x$FTB_VERBOSE = xyes ]; then
     set -x
 fi
 
+# START FIFO
+pipe=/tmp/pipes/beastfeeder-fifo
+if [ ! -p $pipe ];then
+    mkfifo $pipe
+fi
+
 echo "Starting server"
 /usr/java/jdk1.8.0_45/bin/java -server -Xms1024m -Xmx2048m -XX:PermSize=256m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -jar FTBServer-1.7.10-1352.jar nogui
-echo "Server process finished"
